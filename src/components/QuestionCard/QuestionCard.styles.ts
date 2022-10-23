1997ae9dad7b375b68be1205a485bd22950eb92e
Will Leighton <will@fifteenten.co.uk>
2022-10-23 13:38:08 +0100

    button styles

--- src/components/QuestionCard/QuestionCard.styles.ts
+++ src/components/QuestionCard/QuestionCard.styles.ts
@@ -1,10 +1,49 @@
 import styled from "styled-components";
 
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.li`
     max-width: 1100px;
     background-color: #ebfeff;
     border: 2px sold;
     border:  2px solid #0085a3;
     box-shadow: 0px 5px 10px rgba(0,0,0,0.25);
+    padding: 2rem;
+`
+
+
+
+type ButtonWrapperProps = {
+    correct: boolean,
+    userClicked: boolean
+}
+
+
+export const ButtonWrapper = styled.div<ButtonWrapperProps>`
+    transition:  all 0.2s ease-out;
+    :hover{
+        opacity: 0.8;
+    }
+
+    button{
+        cursor: pointer;
+        width: 100%;
+        height: 40px;
+        margin: 5px 0;
+        font-weight: 800;
+        color: white;
+        :disabled{
+            opacity: 0.5;
+        }
+        background: ${({ correct, userClicked }) => 
+            correct
+            ? 'linear-gradient(90deg, #56ffa4, #59bc86)'
+            : !correct && userClicked
+                ? 'linear-gradient(90deg, #ff5656, #c16868)' 
+                :'linear-gradient(90deg, #56ccff, #6eafb4)'
+        };
+
+        border: 3px solid #fff;
+        box-shadow: 1px 2px 0px rgba(0,0,0,0.1);
+        text-shadow: 0px 1px 0px rgba(0,0,0,0.1);
+    }
 `
--- src/components/QuestionCard/QuestionCard.tsx
+++ src/components/QuestionCard/QuestionCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { AnswerObject } from '../../App';
+// styles
+import {Wrapper, ButtonWrapper} from "./QuestionCard.styles";
 
 type Props = {
     question: string;
@@ -13,23 +15,27 @@ type Props = {
 
 const QuestionCard: React.FC<Props> = ({question,answers,callback,userAnswer, questionNr,totalQuestions}) => {
   return (
-      <li role={"article"}>
+      <Wrapper role={"article"}>
           <h2>
               Question: {questionNr} / {totalQuestions}
           </h2>
           <h1 dangerouslySetInnerHTML={{ __html: question }} />
           <div>
               {
-                  answers.map((answer:string, i: number) => (
-                    <div key={i}>
-                        <button disabled={!!userAnswer} onClick={callback}>
-                          <span dangerouslySetInnerHTML={{__html: answer}}/>
-                        </button>
-                    </div>
-                  ))
+                answers.map((answer:string) => (
+                  <ButtonWrapper    
+                    key={answer}
+                    correct={userAnswer?.correctAnswer === answer}
+                    userClicked={userAnswer?.answer === answer}
+                  >
+                    <button disabled={!!userAnswer} value={answer} onClick={callback}>
+                      <span dangerouslySetInnerHTML={{__html: answer}}/>
+                    </button>
+                  </ButtonWrapper>
+                ))
               }
           </div>
-    </li>
+    </Wrapper>
   )
 }
 
