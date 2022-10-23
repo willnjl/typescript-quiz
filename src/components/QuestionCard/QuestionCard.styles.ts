15ada32307c27825c1f60cc2724c063fbcbf9542
Will Leighton <will@fifteenten.co.uk>
2022-10-23 13:12:01 +0100

    move to folder

---
+++ src/components/QuestionCard/QuestionCard.styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div`
+    max-width: 1100px;
+    background-color: #ebfeff;
+    border: 2px sold;
+    border:  2px solid #0085a3;
+    box-shadow: 0px 5px 10px rgba(0,0,0,0.25);
+`
--- src/components/QuestionCard/QuestionCard.tsx
+++ src/components/QuestionCard/QuestionCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AnswerObject } from '../App';
+import { AnswerObject } from '../../App';
 
 type Props = {
     question: string;
