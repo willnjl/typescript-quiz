93b6752126529e5fcc7c4a698fc0154efb84eb61
Will Leighton <will@fifteenten.co.uk>
2022-10-23 11:18:02 +0100

    scalfholding

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React from 'react'
+import QuestionCard from './components/QuestionCard'
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
-    <div className="App">
-      Quiz
+    <div>
+      <h1>App</h1>
+      <button className="start" onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>
+        Score:
+      </p>
+      <p>
+        ...Loading Questions
+      </p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
